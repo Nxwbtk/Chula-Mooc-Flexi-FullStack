c3363f6d846ec95396f0c1fdbf43f0c7884b59c3
NewDev <65420147@kmitl.ac.th>
2024-11-05 04:33:37 +0700

    Setup vue with Nuxt

---
+++ Frontend/app/app.vue
@@ -0,0 +1,27 @@
+<!-- Function here -->
+<!-- Must add setup for use useHead function this is nuxt provided -->
+<script setup>
+useHead({
+  title: "Test"
+})
+</script>
+
+<!-- UI here -->
+<template>
+  <!-- <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div> -->
+  hello
+</template>
+
+
+<!-- Style here -->
+<style lang="css">
+body {
+  background-color: aqua;
+}
+</style>
+
+
+<!-- https://www.mycourseville.com/?q=onlinecourse/course/51618 -->
